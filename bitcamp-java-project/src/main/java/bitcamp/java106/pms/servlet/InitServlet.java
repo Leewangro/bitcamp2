bcb64515cd469bb07e97dd1301dcfc2c43cbcaf1
Leewangro <dldhkdfh@naver.com>
2018-05-16 14:02:20 +0900

    'king'

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/InitServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/InitServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import bitcamp.java106.pms.Appconfig;
+import bitcamp.java106.pms.AppConfig;
 
 // 하는 일:
 // => Spring IoC 컨테이너(bean container)를 준비한다. 
@@ -27,10 +27,15 @@ public class InitServlet extends HttpServlet {
     
     @Override
     public void init() throws ServletException {
-        iocContainer = new AnnotationConfigApplicationContext(Appconfig.class);
+        iocContainer = new AnnotationConfigApplicationContext(AppConfig.class);
     }
     
     // 다른 서블릿이 사용할 자원을 준비하는 일만 하기 때문에 
     // 굳이 클라이언트의 요청을 처리할 service() 메서드를 구현할 필요가 없다.
 }
+
+
+
+
+
 
