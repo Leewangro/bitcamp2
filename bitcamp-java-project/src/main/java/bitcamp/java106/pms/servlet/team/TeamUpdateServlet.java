1c4b7dafcd0f4643b169e627ac276b9ac60eacc4
kimbokjin <kbj3182@gmail.com>
2018-05-16 16:41:31 +0900

    team update

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamUpdateServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamUpdateServlet.java
@@ -1,48 +1,69 @@
 // Controller 규칙에 따라 메서드 작성
 package bitcamp.java106.pms.servlet.team;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Date;
 
-import org.springframework.stereotype.Component;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import bitcamp.java106.pms.controller.Controller;
 import bitcamp.java106.pms.dao.TeamDao;
 import bitcamp.java106.pms.domain.Team;
-import bitcamp.java106.pms.server.ServerRequest;
-import bitcamp.java106.pms.server.ServerResponse;
+import bitcamp.java106.pms.servlet.InitServlet;
 
-@Component("/team/update")
-public class TeamUpdateController implements Controller {
+@SuppressWarnings("serial")
+@WebServlet("/team/update")
+public class TeamUpdateServlet extends HttpServlet {
 
     TeamDao teamDao;
     
-    public TeamUpdateController(TeamDao teamDao) {
-        this.teamDao = teamDao;
+    @Override
+    public void init() throws ServletException {
+        teamDao = InitServlet.getApplicationContext().getBean(TeamDao.class);
     }
 
     @Override
-    public void service(ServerRequest request, ServerResponse response) {
-        PrintWriter out = response.getWriter();
-        
+    protected void doPost(
+            HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         Team team = new Team();
         team.setName(request.getParameter("name"));
         team.setDescription(request.getParameter("description"));
         team.setMaxQty(Integer.parseInt(request.getParameter("maxQty")));
         team.setStartDate(Date.valueOf(request.getParameter("startDate")));
         team.setEndDate(Date.valueOf(request.getParameter("endDate")));
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+        out.println("<title>팀 변경</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>팀 변경 결과</h1>");
         
         try {
             int count = teamDao.update(team);
             if (count == 0) {
-                out.println("해당 이름의 팀이 없습니다.");
+                out.println("<p>해당 이름의 팀이 없습니다.</p>");
             } else {
-                out.println("변경하였습니다.");
+                out.println("<p>변경하였습니다.</p>");
             }
         }  catch (Exception e) {
-            out.println("변경 실패!");
+            out.println("<p>변경 실패!</p>");
             e.printStackTrace(out);
         }
+        out.println("</body>");
+        out.println("</html>");
     }
 }
 
