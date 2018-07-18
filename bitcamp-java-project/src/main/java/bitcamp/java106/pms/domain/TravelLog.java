dd6f984911cc5b2ba114c212e560409a65891210
zx3620 <zx3620@gmail.com>
2018-07-18 21:51:59 +0900

    joongi

---
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/domain/TravelLog.java
@@ -0,0 +1,64 @@
+package bitcamp.java106.pms.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class TravelLog implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int no;
+    private String member;
+    private String partner;
+    private String preference;
+    
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date startDate;
+    private Date endDate;
+    @Override
+    public String toString() {
+        return "TravelLog [no=" + no + ", member=" + member + ", partner=" + partner + ", preference=" + preference
+                + ", startDate=" + startDate + ", endDate=" + endDate + "]";
+    }
+    public int getNo() {
+        return no;
+    }
+    public void setNo(int no) {
+        this.no = no;
+    }
+    public String getMember() {
+        return member;
+    }
+    public void setMember(String member) {
+        this.member = member;
+    }
+    public String getPartner() {
+        return partner;
+    }
+    public void setPartner(String partner) {
+        this.partner = partner;
+    }
+    public String getPreference() {
+        return preference;
+    }
+    public void setPreference(String preference) {
+        this.preference = preference;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    
+    
+}
