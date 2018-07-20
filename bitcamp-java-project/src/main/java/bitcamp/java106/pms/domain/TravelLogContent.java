e3eeffdc585f80378a8b47b8d9861297d28f79cb
zx3620 <zx3620@gmail.com>
2018-07-20 17:57:08 +0900

    joongi

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/domain/TravelLogContent.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/domain/TravelLogContent.java
@@ -15,47 +15,56 @@ public class TravelLogContent implements Serializable {
 	private String address;
 	private String photo;
 	private String review;
-	@Override
-	public String toString() {
-		return "TravelLogContent [tlno=" + tlno + ", tcno=" + tcno + ", place=" + place + ", address=" + address
-				+ ", photo=" + photo + ", review=" + review + "]";
-	}
-	public int getTlno() {
-		return tlno;
-	}
-	public void setTlno(int tlno) {
-		this.tlno = tlno;
-	}
-	public int getTcno() {
-		return tcno;
-	}
-	public void setTcno(int tcno) {
-		this.tcno = tcno;
-	}
-	public String getPlace() {
-		return place;
-	}
-	public void setPlace(String place) {
-		this.place = place;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getPhoto() {
-		return photo;
-	}
-	public void setPhoto(String photo) {
-		this.photo = photo;
-	}
-	public String getReview() {
-		return review;
-	}
-	public void setReview(String review) {
-		this.review = review;
-	}
+	private String day;
+    @Override
+    public String toString() {
+        return "TravelLogContent [tlno=" + tlno + ", tcno=" + tcno + ", place=" + place + ", address=" + address
+                + ", photo=" + photo + ", review=" + review + ", day=" + day + "]";
+    }
+    public int getTlno() {
+        return tlno;
+    }
+    public void setTlno(int tlno) {
+        this.tlno = tlno;
+    }
+    public int getTcno() {
+        return tcno;
+    }
+    public void setTcno(int tcno) {
+        this.tcno = tcno;
+    }
+    public String getPlace() {
+        return place;
+    }
+    public void setPlace(String place) {
+        this.place = place;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhoto() {
+        return photo;
+    }
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    public String getReview() {
+        return review;
+    }
+    public void setReview(String review) {
+        this.review = review;
+    }
+    public String getDay() {
+        return day;
+    }
+    public void setDay(String day) {
+        this.day = day;
+    }
+	
+	
 
 
 }
