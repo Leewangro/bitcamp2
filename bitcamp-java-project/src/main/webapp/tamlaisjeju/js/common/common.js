f23e6e601e753fdc8a184ac8e384623ad2201770
zx3620 <zx3620@gmail.com>
2018-07-27 17:47:25 +0900

    # WARNING: head commit changed in the meantime
    joongi

--- bitcamp-java-project/src/main/webapp/tamlaisjeju/js/common/common.js
+++ bitcamp-java-project/src/main/webapp/tamlaisjeju/js/common/common.js
@@ -1,2 +1,2 @@
-var serverRoot = "http://localhost:8888/bitcamp-java-project";
-//var serverRoot = "http://tamlaisjeju.java106.com:8888/bitcamp-java-project";
+//var serverRoot = "http://localhost:8888/bitcamp-java-project";
+var serverRoot = "http://tamlaisjeju.java106.com:8888/bitcamp-java-project";
