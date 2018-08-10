54de43950572386395d63e3458f9259069c8ed97
gilnami <gilnami.kim@gmail.com>
2018-08-10 11:33:12 +0900

    'gilnam'

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/web/json/SNSMemberController.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/web/json/SNSMemberController.java
@@ -1,11 +1,10 @@
-package bitcamp.java106.pms.web;
+package bitcamp.java106.pms.web.json;
 
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import bitcamp.java106.pms.domain.SNSMember;
 import bitcamp.java106.pms.service.SNSMemberService;
