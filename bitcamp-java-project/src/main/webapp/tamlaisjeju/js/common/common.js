ee5f8d365f3fc085059cde8b88a2c29189fe2007
kimakuh <kimakuh@gmail.com>
2018-08-16 16:49:24 +0900

    garam

--- bitcamp-java-project/src/main/webapp/tamlaisjeju/js/common/common.js
+++ bitcamp-java-project/src/main/webapp/tamlaisjeju/js/common/common.js
@@ -1,6 +1,6 @@
-//var serverRoot = "http://localhost:8888/bitcamp-java-project";
-// var serverRoot = "http://tamlaisjeju.java106.com:8888/bitcamp-java-project";
-var serverRoot = "http://jungjoongi.com:8888/bitcamp-java-project";
+var serverRoot = "http://localhost:8888/bitcamp-java-project";
+//var serverRoot = "http://tamlaisjeju.java106.com:8888/bitcamp-java-project";
+//var serverRoot = "http://jungjoongi.com:8888/bitcamp-java-project";
 
 
 
